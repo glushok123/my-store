92859488d7c36e7181f6502afb5dc6cd83866550
andrey <glushok19999@gmail.com>
2023-02-11 18:08:18 +0300

    поисковая строка

---
+++ resources/views/layouts/search.blade.php
@@ -0,0 +1,25 @@
+<div class='container'>
+    <br>
+
+    <div class="row height d-flex justify-content-center align-items-center">
+        <div class="col-md-6">
+          <div class="form">
+            <i class="fa fa-search"></i>
+            <input type="text" class="form-control form-input" placeholder="Я ищу...">
+            <span class="left-pan"><i class="fa fa-microphone"></i></span>
+          </div>
+        </div>
+      </div>
+  
+      <br>
+  
+      <div class="row height d-flex justify-content-center align-items-center">
+        <div class="col-md-6">
+            <span class="btn btn-outline-secondary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+                Категории
+            </span>
+        </div>
+      </div>
+
+      <br>
+</div>
